f98674a60e431c463232d3562465332ed86a1273
krausware <jkrausfeldt1@gmail.com>
2021-01-18 19:54:01 +0000

    end of day

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
-
+	
 
 
  # CSV: OLD Rule name, OLD VulID, OLD RulID, OLD_RulID_rule, NEW VulID, NEW RulID 
 import csv
-
+import xml.etree.ElementTree as ET
 
 
 
@@ -15,49 +15,36 @@ old_check = input("Enter the old checklist location")
 new_check = input ("Enter new checklist location")
     
 
-# open(old_check) as oldf
+#with open(old_check) as oldf:
 
 
-# open(new_check) as newf
-    
-    
-# CSV input validation module
-# Checklist input validation module   
-
-    
-# read the first line and save the variables
-
-with open(csvmap) as csvfile:
+#with open(new_check) as newf:
        
-    f = csv.reader(csvfile, delimiter=',')
-    
-    oldRulName=[]
-    oldVulID=[]
-    oldRulID=[]
-    oldRulID_rule=[]
-    newVulID=[]
-    newRulID=[]
-    
-    for row in f:
-        
-        
-        oldRulName.append(row[0])
-        oldVulID.append(row[1])
-        oldRulID.append(row[2])
-        oldRulID_rule.append(row[3])
-        newVulID.append(row[4])
-        newRulID.append(row[5])
-        
-        
-        
-        
-        
+with open(csvmap) as csvfile:
+ f = csv.reader(csvfile, delimiter=',')
+ oldRulName=[]
+ oldVulID=[]
+ oldRulID=[]
+ oldRulID_rule=[]
+ newVulID=[]
+ newRulID=[]
+ y=0
 
+ for row in f:
+        
+        
+  oldRulName.append(row[0])
+  oldVulID.append(row[1])
+  oldRulID.append(row[2])
+  oldRulID_rule.append(row[3])
+  newVulID.append(row[4])
+  newRulID.append(row[5])        	 
+  print(oldVulID[y])
+  y +=1
 
-# find the rule
+	#  
         
-        
-        
+    s      
         
         
 
@@ -71,18 +58,18 @@ with open(csvmap) as csvfile:
         
     
     #may need to strip some data
-    print(oldRulName)   
-    with open(old_check) as oldreader
+    # print(oldRulName)   
+    #    with open(old_check) as oldreader
 
-    with open(new_check) as newreader
+    #   with open(new_check) as newreader
     
-     for indID in oldVulID
+#     for indID in oldVulID
      
-        #search for old tags
+       #search for old tags
         
-        #captureold data
-        oStatus = 
-        oComments = fin
+       #        #captureold data
+       #        oStatus = 
+       #        oComments = fin
         
         #write to new data
         
